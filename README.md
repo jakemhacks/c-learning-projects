6ce11be4c75cff85f7f2a44e3a6af6ac609e426f
Jake <jakemhacks@gmail.com>
2025-12-06 14:07:45 -0600

    initial commit and hello world

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# c-learning-projects
+# C Learning Projects
+This is a series of progressively more complex projects I am making in order to learn C.
+
+## Project List
+1. [Hello World](/hello/hello.c)
---
+++ hello/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// I'll make it a bit more interesting with a hello function
+int hello(void) {
+    printf("Hello, World\n");
+}
+
+int main(void) {
+    hello();
+    return 0;
+}
